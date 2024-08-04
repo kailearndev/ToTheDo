d605bafc374513e34d354caf01093a6f4602fd3f
kai dev <kaidev.contact@gmail.com>
2024-08-05 00:16:52 +0700

    Initial Commit

---
+++ ToTheDo/ToTheDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToTheDoApp.swift
+//  ToTheDo
+//
+//  Created by kai on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ToTheDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
